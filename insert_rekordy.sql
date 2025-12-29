3641cdee6575dd0ecc9c2af2a9e35d96b0253550
Piotr Duch <piotr.duch@itm.com.pl>
2025-12-29 22:00:59 +0100

    dodanie skryptow i exe

---
+++ insert_rekordy.sql
@@ -0,0 +1,8 @@
+INSERT INTO dbo.ExportHistory (ExportName, ExportDateTime, UserName, LocationName)
+VALUES
+(N'Eksport sprzedaży', '2025-01-05 10:15:00', N'jan.kowalski', N'Lokal 1'),
+(N'Eksport magazynu',  '2025-01-06 08:02:00', N'jan.kowalski', N'Lokal 2'),
+(N'Eksport sprzedaży', '2025-01-06 12:30:00', N'anna.nowak',   N'Lokal 1'),
+(N'Eksport faktur',    '2025-01-07 16:45:00', N'anna.nowak',   N'Lokal 3'),
+(N'Eksport raportu',   '2025-01-10 09:00:00', N'piotr.ziel',   N'Lokal 2');
+GO
